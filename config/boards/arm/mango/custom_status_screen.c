067238ca6b72f60c0dcfe688d506745ae02edd1f
MaxDac <massimiliano.dacunzo@hotmail.com>
2024-06-05 21:14:42 +0100

    Moved to Corneish

--- config/boards/arm/mango/custom_status_screen.c
+++ config/boards/arm/mango/custom_status_screen.c
@@ -1,16 +1,21 @@
+/*
+ *
+ * Copyright (c) 2021 Darryl deHaan
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
 #include "widgets/battery_status.h"
 #include "widgets/peripheral_status.h"
 #include "widgets/output_status.h"
-#include "widgets/profile_status.h"
 #include "widgets/layer_status.h"
 #include "custom_status_screen.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-// LOGOS
-LV_IMG_DECLARE(keyboard_logo);
-LV_IMG_DECLARE(user_logo);
+LV_IMG_DECLARE(zenlogo);
+LV_IMG_DECLARE(layers2);
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
 static struct zmk_widget_battery_status battery_status_widget;
@@ -20,16 +25,12 @@ static struct zmk_widget_battery_status battery_status_widget;
 static struct zmk_widget_output_status output_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PROFILE_STATUS)
-static struct zmk_widget_profile_status profile_status_widget;
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
+static struct zmk_widget_peripheral_status peripheral_status_widget;
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
 static struct zmk_widget_layer_status layer_status_widget;
-#endif
-
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
-static struct zmk_widget_peripheral_status peripheral_status_widget;
 #endif
 
 lv_obj_t *zmk_display_status_screen() {
@@ -39,23 +40,25 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_MID, 0, 2);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 41);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
     zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_MID, 0,
+                 41);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
     lv_obj_t *LayersHeading;
     LayersHeading = lv_img_create(screen);
     lv_obj_align(LayersHeading, LV_ALIGN_BOTTOM_MID, 0, -30);
+    lv_img_set_src(LayersHeading, &layers2);
 
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
@@ -64,20 +67,10 @@ lv_obj_t *zmk_display_status_screen() {
 #endif
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    lv_obj_t *keyboard_logo_icon;
-    keyboard_logo_icon = lv_img_create(screen);
-    lv_img_set_src(keyboard_logo_icon, &keyboard_logo);
-    lv_obj_align(keyboard_logo_icon, LV_ALIGN_LEFT_MID, 50, 0);
-
-    lv_obj_t *user_logo_icon;
-    user_logo_icon = lv_img_create(screen);
-    lv_img_set_src(user_logo_icon, &user_logo);
-    lv_obj_align(user_logo_icon, LV_ALIGN_RIGHT_MID, 0, 0);
-#endif
-
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PROFILE_STATUS)
-    zmk_widget_profile_status_init(&profile_status_widget, screen);
-    lv_obj_align(zmk_widget_profile_status_obj(&profile_status_widget), LV_ALIGN_LEFT_MID, 35, 0);
+    lv_obj_t *zenlogo_icon;
+    zenlogo_icon = lv_img_create(screen);
+    lv_img_set_src(zenlogo_icon, &zenlogo);
+    lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 0, -5);
 #endif
 
     return screen;
